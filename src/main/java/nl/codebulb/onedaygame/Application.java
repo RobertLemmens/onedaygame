77d85450c29a26afd06c6bd8ec15dc775699bec4
Robert Lemmens <rj.lemmens@pm.me>
2022-06-20 09:56:37 +0200

    snapshot

--- src/main/java/nl/codebulb/onedaygame/Application.java
+++ src/main/java/nl/codebulb/onedaygame/Application.java
@@ -4,7 +4,7 @@ import nl.daedalus.engine.core.DaedalusApplication;
 import nl.daedalus.engine.core.DaedalusOptions;
 
 /**
- * A game made in a day in my custom 2d engine Daedalus.
+ * A game made in a day in my custom 2d engine.
  *
  * We are just figuring out the engine API and looking for the spots that need work
  * and features we might want to work on next. Actually building something in the engine is a good way of doing that.
